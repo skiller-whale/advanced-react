90b84cca921aa21908387d6779851181f5440a7f
Hywel Carver <>
2020-05-20 22:10:07 +0100

    Refs

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom"
 import App from "./App"
 // import App from "./hooks/useState"
 // import App from "./hooks/useEffect"
+// import App from "./refs/refClassComponent"
+// import App from "./refs/refFunctionComponent"
+// import App from "./refs/childRefs"
 
 const root = document.getElementById("root")
 
